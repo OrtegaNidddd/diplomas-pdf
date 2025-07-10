8008f44e36ab7782b0e52029909f0a9764c26e8c
Nick Ortega <orteganid@gmail.com>
2025-07-09 20:15:46 -0500

    Fonts de manera local

--- resources/views/primer-diploma.blade.php
+++ resources/views/primer-diploma.blade.php
@@ -7,8 +7,16 @@
     <!-- Scripts -->
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+        
+        @font-face {
+            font-family: 'Montserrat Alternates';
+            src: url('{{ asset('fonts/MontserratAlternates-Regular.ttf') }}') format('truetype');
+        }
+
+        @font-face {
+            font-family: 'Great vibes';
+            src: url('{{ asset('fonts/GreatVibes-Regular.ttf') }}') format('truetype');
+        }
 
         .font-title {
             font-family: 'Montserrat Alternates', serif;
