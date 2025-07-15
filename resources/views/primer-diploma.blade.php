775db0376882d2bdf33bb2d7e057855860c73f07
Nick Ortega <orteganid@gmail.com>
2025-07-15 09:25:18 -0500

    Estilos CSS puro actualizado

--- resources/views/primer-diploma.blade.php
+++ resources/views/primer-diploma.blade.php
@@ -6,115 +6,341 @@
     <title>Diploma FESC</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
+        
+        .font-sans {
+            font-family: 'Arial', sans-serif;
+        }
+
+        .font-serif {
+            font-family: 'Times New Roman', serif;
+        }
+
+        .h-full{
+            height: 100%;
+        }
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            background-color: #f3f4f6; /* Tailwind's gray-100 */
+        }
     
-    
+
+        .principal-container {
+            width: 100%;
+            margin: auto;
+            background-color: #fff; 
+        }
+
+        .contenedor-diploma {
+            width: 100%;
+            position: relative;
+            background-color: #fff;
+        }
+
+        .esquina-superior-derecha{
+            position: absolute;
+            top: 0;
+            right: 0;
+            z-index: 40;
+        }
+
+        .esquina-superior-izquierda{
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 40;
+        }
+
+        .esquina-inferior-derecha{
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            z-index: 40;
+        }
+
+        .esquina-inferior-izquierda{
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            z-index: 40;
+        }
+
+        .logo-fesc-proyectando {
+            position: absolute;
+            top: 1rem;
+            left: 50%;
+            transform: translateX(-50%);
+            z-index: 50;
+        }
+
+        .logo-fesc-proyectando img{
+            width: 20rem;
+            height: auto;
+        }
+
+        .logo-mineducacion {
+            position: absolute;
+            top: 11rem;
+            left: -1.5rem;
+            transform: translateY(-50%) rotate(-90deg);
+            z-index: 50;
+        }
+
+        .logo-mineducacion span {
+            border-top: 2px solid #374151; 
+            border-bottom: 2px solid #374151; 
+            font-size: 7px; 
+            color: #374151; 
+        }
+
+        .contenido-principal {
+            position: relative;
+            z-index: 30;
+            display: flex;
+            flex-direction: column;
+            gap: 1.5rem;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+        }
+
+        .contenido-diploma {
+            position: relative;
+            z-index: 20;
+            width: 100%;
+        }
+
+        .titulo-principal{
+            position: absolute;
+            top: -10rem;
+            left: 50%;
+            transform: translateX(-50%);
+            text-align: center;
+            font-size: 1.25rem;
+            line-height: 1.75rem;
+            color: #1f2937;
+            text-align: center;
+            width: 100%;
+            font-style: italic;
+        }
+
+        .certifican{
+            position: absolute;
+            top: -7rem;
+            left: 50%;
+            transform: translateX(-50%);
+            font-size: 1.5rem /* 24px */;
+            line-height: 2rem /* 32px */;
+            text-align: center;
+            color: #1f2937;
+            width: 100%;
+            font-style: italic;
+        }
+
+        .nombre{
+            position: absolute;
+            top: -4rem;
+            left: 50%;
+            transform: translateX(-50%);
+            font-size: 3rem /* 48px */;
+            line-height: 1;
+            width: 75%;
+            text-align: center;
+            color: #1f2937;
+            border-bottom-width: 2px;
+            border-color: #6b7280;
+            font-style: italic;
+            white-space: nowrap;
+        }
+
+        .documento{
+            position: absolute;
+            top: -0.5rem;
+            left: 50%;
+            transform: translateX(-50%);
+            font-size: 1.5rem /* 24px */;
+            line-height: 2rem /* 32px */;
+            text-align: center;
+            color: #1f2937;
+            width: 100%;
+        }
+
+        .asistio{
+            position: absolute;
+            top: 2.5rem;
+            left: 50%;
+            transform: translateX(-50%);
+            font-size: 1.25rem /* 20px */;
+            line-height: 1.75rem /* 28px */;
+            color: #1f2937;
+            text-align: center;
+            width: 100%;
+            font-style: italic;
+        }
+
+        .nombre-evento{
+            position: absolute;
+            top: 4rem;
+            left: 50%;
+            transform: translateX(-50%);
+            font-size: 3rem /* 48px */;
+            line-height: 1;
+            text-align: center;
+            white-space: nowrap;
+            color: #1f2937;
+            font-weight: 500; /* Medium */
+        }
+
+        .participacion{
+            position: absolute;
+            top: 8rem;
+            left: 50%;
+            transform: translateX(-50%);
+            font-size: 1.5rem /* 24px */;
+            line-height: 2rem /* 32px */;
+            text-align: center;
+            color: #1f2937;
+            width: 100%;
+            font-style: italic;
+        }
+
+        .fecha-realizacion{
+            position: absolute;
+            top: 13rem;
+            left: 50%;
+            transform: translateX(-50%);
+            font-size: 1.25rem /* 20px */;
+            line-height: 1.75rem /* 28px */;
+            text-align: center;
+            color: #1f2937;
+            width: 100%;
+            font-style: italic;
+        }
+
+        .contenedor-firma {
+            position: absolute;
+            top: 23rem;
+            left: 50%;
+            transform: translateX(-50%);
+            width: 16%;
+            text-align: center;
+        }
+
+        .nombre-firma{
+            white-space: nowrap;
+        }
+
+        .nombre-firma strong{
+            border-top-width: 2px;
+            border-color: #000;
+            padding-top: 0.25rem;
+        }
+
+        .imagen-firma{
+            position: relative;
+            top: 16rem;
+            left: 1rem;
+            z-index: 50;
+        
+        }
+
+        .firma{
+            width: 12rem; /* Adjust as needed */
+            height: auto; /* Maintain aspect ratio */
+        }
     </style>
 </head>
-<body class="h-full bg-gray-100">
-
-    <div class="bg-white m-auto h-full w-full ">
-        
-        <div class="relative h-full w-full bg-white">
-            
-            <div class="absolute top-[0] right-[0] z-40 ">
+<body class="h-full">
+    
+    <!-- Contenedor principal del diploma -->
+    <div class="principal-container h-full">
+        <!-- Contenedor del diploma -->
+        <div class="h-full contenedor-diploma">
+            <!-- Esquina superior derecha -->
+            <div class="esquina-superior-derecha">
                 <!-- SVG Esquina superior derecha -->
-                <svg width="144" height="89" viewBox="0 0 144 89" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M113 0H89C84.5 33.5 101.662 55.5579 144 70.5V47.5C125 33.5 113 22 113 0Z" fill="#2377B3"/>
-                    <path d="M89.5 0H0C37.5 51.5 71.5 78 144 88.5V66C104.069 52.4015 88 34 89.5 0Z" fill="#3A88C8"/>
-                </svg>
+                <img src="{{ asset('images/Right-top-corner.png') }}" alt="Esquina superior derecha" >
             </div>
             
-            <div class="absolute top-[0] left-[0] z-40">
+            <div class="esquina-superior-izquierda">
                 <!-- SVG Esquina superior izquierda -->
-                <svg width="160" height="114" viewBox="0 0 160 114" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M33 0H0V51C57.5 60 83.3089 34.0368 118 0C91.5002 26 60 24 33 0Z" fill="#9E1E1D"/>
-                    <path d="M0 51V114C85 101 130 64 160 0H118C78 35.5 48 46 0 51Z" fill="#C31A1D"/>
-                </svg>
+                <img src="{{ asset('images/Left-top-corner.png') }}" alt="Esquina superior izquierda" >
             </div>
 
-            <div class="absolute bottom-[0px] right-[0px] z-40">
+            <div class="esquina-inferior-derecha">
                 <!-- SVG Esquina inferior derecha -->
-                <svg width="164" height="180" viewBox="0 0 164 180" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M34 180.002C193.5 142.501 157.5 130.5 164 12C128 -9.50037 178 129.501 0 180.002H34Z" fill="#9C1F1D"/>
-                    <path d="M164 15V12L162 13L164 15Z" fill="#9C1F1D"/>
-                    <path d="M164 180H34C195.5 136.353 136.5 5.41447 164 14.4449V180Z" fill="#C31A1D"/>
-                    <path d="M164 4.60691V9.99983C160.5 9.5 159.581 8.01785 160.155 5.68549C160.541 4.60707 162.5 3.49995 164 4.60691Z" fill="#C41C1C"/>
-                    <ellipse cx="155" cy="3.50013" rx="3" ry="2.5" transform="rotate(11 155 3.50013)" fill="#C41C1C"/>
-                </svg>
+                <img src="{{ asset('images/Bottom-right-corner.png') }}" alt="Esquina inferior derecha" >
 
             </div>
             
-            <div class="absolute bottom-[0] left-[0] z-40">
+            <div class="esquina-inferior-izquierda">
                 <!-- SVG Esquina inferior izquierda -->
-                <svg width="319" height="116" viewBox="0 0 319 116" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M50.9599 12.4992C58.0131 8.98635 39.8763 91.7875 89.2487 81.2481C24.9996 116 9.21076 77.4118 28.0002 37C35.0506 23.954 41.3877 13.5028 50.9599 12.4992Z" fill="#3987C9"/>
-                    <path d="M319 116H237.873C131.203 37 6.00648 147.5 0 91V71C6.00942 54.5 18.529 41.5 28.044 37C3.00471 195 164.758 -47.5 319 116Z" fill="#30A2AC"/>
-                    <path d="M74.5 116H69C122.5 79 185.5 76 215.5 116H179C152 105 114.5 99.5002 74.5 116Z" fill="#3987C9"/>
-                    <path d="M0 116V90.0001C11 145.5 129 37 238.5 116H215.5C185 76 121.5 81.4999 69 116H0Z" fill="#108982"/>
-                    <path d="M78.0646 116H73.5C118.777 99.1082 141.569 100.917 179.5 116H139.433C118.639 111.98 100.38 111.478 78.0646 116Z" fill="#2377B3"/>
-                    <path d="M64.5 57.5C74 70.5 90.5 70.5 107.5 65C91 80.5 66 76 64.5 57.5Z" fill="#2EA3AC"/>
-                    <ellipse cx="55.0002" cy="6.0318" rx="3.46083" ry="4.1413" transform="rotate(28 55.0002 6.0318)" fill="#2EA3AC"/>
-                </svg>
+                <img src="{{ asset('images/Bottom-left-corner.png') }}" alt="Esquina inferior izquierda" >
             </div>
 
             <!-- Logo FESC & PROYECTANDO -->
-            <div class="absolute top-4 left-1/2 transform -translate-x-1/2 z-50">
-                <img src="images/LOGO_PROYECTANDO.png" alt="Logo FESC" class="w-80 h-auto ">
+            <div class="logo-fesc-proyectando">
+                <img src="{{ asset('images/LOGO_PROYECTANDO.png') }}" alt="Logo FESC">
             </div>
 
             <!-- Logo MinEducacion lateral izquierdo -->
-            <div class="absolute -left-6 top-44 transform -translate-y-1/2 -rotate-90 z-50">
-                <span class="border-t-2 border-b-2 border-gray-700 text-[7px] text-gray-700">VIGILADA MINEDUCACIÓN</span>
+            <div class="logo-mineducacion">
+                <span>VIGILADA MINEDUCACIÓN</span>
             </div>
 
             <!-- Contenido principal -->
-            <div class="relative z-30 h-full flex flex-col gap-6 justify-center items-center w-full ">
+            <div class=" h-full contenido-principal">
                 <!-- Contenido del diploma -->
-                <div class="relative z-10 w-full">
+                <div class="contenido-diploma">
                     <!-- Título principal -->
-                    <h1 class="absolute -top-40 left-1/2 transform -translate-x-1/2 text-xl text-gray-800 text-center w-full  italic font-serif">
+                    <h1 class="titulo-principal font-serif">
                         Proyectando y la Fundación de Estudios Superiores Comfanorte - FESC
                     </h1>
 
-                    <span class="absolute -top-28 left-1/2 transform -translate-x-1/2 text-2xl text-gray-800 text-center w-full italic font-serif">
+                    <span class="certifican font-serif">
                         Certifican que:
                     </span>
                     
                     <!-- Nombre centrado dinámicamente -->
-                    <h1 class="absolute -top-16 left-1/2 transform -translate-x-1/2 text-5xl w-3/4 text-center border-b-2 border-gray-500 whitespace-nowrap font-serif italic">Nick Alejandro Ortega Mendez</h1>
+                    <h1 class="nombre font-serif ">Nick Alejandro Ortega Mendez</h1>
 
                     <!-- Documento de Identidad -->
-                    <span class="absolute -top-2 left-1/2 transform -translate-x-1/2 text-2xl text-gray-800 text-center w-full font-sans">
+                    <span class="documento font-sans">
                         D.I. 1091357666
                     </span>
                     
                     <!-- Asistió al -->
-                    <span class="absolute top-10 left-1/2 transform -translate-x-1/2 text-xl text-gray-800 text-center w-full italic font-serif">
+                    <span class="asistio font-serif">
                         Asistió al
                     </span>
                     
                     <!-- Nombre del evento -->
-                    <h1 class="absolute top-16 left-1/2 transform -translate-x-1/2 text-5xl text-center whitespace-nowrap font-sans font-medium">V Congreso Internacional Proyectando</h1>
+                    <h1 class="nombre-evento font-sans">V Congreso Internacional Proyectando</h1>
 
                     <!-- Texto de participación centrado -->
-                    <span class="absolute top-32 left-1/2 transform -translate-x-1/2 text-2xl text-gray-800 text-center w-full italic font-serif">
+                    <span class="participacion font-serif">
                         "Impulsamos el Desarrollo Turístico y Productivo de Norte de Santander 
                         <br> 
                         a través de la educación superior."
                     </span>
 
                     <!-- Fechas de realización -->
-                    <h1 class="absolute top-52 left-1/2 transform -translate-x-1/2 text-xl text-gray-800 text-center w-full italic font-sans">
+                    <h1 class="fecha-realizacion font-sans">
                         Realizado los dias 21, 22, 23 y 24 de octubre de 2025 en la ciudad de San José de Cúcuta, Colombia.
                         <br>
                         Se firma a los 10 días del mes de noviembre de 2025.
                     </h1>
 
                     <!-- Contenedor para los dos textos de firma -->
-                    <div class="absolute top-[23rem] left-1/2 transform -translate-x-1/2 w-1/6 text-center">
+                    <div class="contenedor-firma">
 
-                        <span class="font-sans whitespace-nowrap ">
-                            <strong class=" border-t-2 border-black pt-1">Carmen Cecilia Quero de González</strong>
+                        <span class="font-sans nombre-firma">
+                            <strong>Carmen Cecilia Quero de González</strong>
                             <br>
                             Rectora FESC
                         </span>
@@ -122,7 +348,7 @@
                 </div>
                 <!-- Imagen de la firma -->
                 <div class="relative top-64 left-4 z-50">
-                    <img src="images/firma_ejemplo.png" alt="" class="w-48 h-auto">
+                    <img src="{{ asset('images/firma_ejemplo.png') }}" alt="" class="firma">
                 </div>
             </div>
         </div>
